c6ba9735ff63671e873b235a2bb91ce89d813206
Eylem Aytas <eylem.aytas@hotmail.com>
2023-04-12 00:36:14 -0400

    Modifies index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 12
+let num4 = 3
+let mod = num3 / num4
+console.log(mod)
+
+
+let max = Math.max(1,2,20)
